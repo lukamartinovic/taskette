844901e08a4cfc59fd2b308591be2e36eeeede9c
Luka <lukamartinovic@gmail.com>
2019-05-27 21:59:15 +0200

    Added authentication middleware

--- jeera-napredno-web/middleware/user.js
+++ jeera-napredno-web/middleware/user.js
@@ -2,21 +2,49 @@ const mongoose = require('mongoose');
 const userSchema = require('../schema/User');
 const taskSchema = require('../schema/Task');
 const projectSchema = require('../schema/Project');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = mongoose.model('User', userSchema);
 
-module.exports.addUser = function (req, res, next) {
-    const newUser = new User(
+module.exports.addUser = async function (req, res, next) {
+
+    const newUser =
         {
             email: req.body.email,
             password: req.body.password,
             role: req.body.role,
             level: req.body.level
-        }
-    );
+        };
 
-    newUser.save
-        .then((doc) => {return res.send(doc)})
-        .catch((err) => {return next(err)})
+    try{
+        const doc = await User.create(newUser);
+        res.send(doc);
+    } catch(err) {return next(err);}
+};
+
+module.exports.login = async function (req, res, next) {
+     try{
+         let loginErr = "Incorrect email or password";
+         const user = await User.findOne({email: { $eq: req.body.email}});
+         if(!user) throw loginErr;
+         const compare = await bcrypt.compare(req.body.password, user.password);
+         if(!compare) throw loginErr;
+         const token = jwt.sign(
+             {email:user.email, role: user.role, level: user.level},
+             process.env.SECRET,
+             {expiresIn:"1d"}
+
+         );
+         res.send(token);
+     }
+     catch(err){return next(err)}
+};
+
+module.exports.authenticate = async function(req, res, next){
+    try {
+        await jwt.verify(req.body.token, process.env.SECRET);
+        next();
+    } catch(err) {return next(err)};
 };
 
 
